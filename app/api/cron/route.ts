1a3d19c5e56cc8b30562554c35f2f8e9307e73d7
WinnardArthur <winnardarthur7@gmail.com>
2024-04-03 16:51:17 +0000

    fix deployment errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,6 @@ import {
 } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
